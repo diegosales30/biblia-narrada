848e4faea801a25ca292459770e18d5214dafc2d
Diego sales <diegofelipesales23@gmail.com>
2024-05-15 15:58:27 -0400

    feat: criando função para abrir e fechar modal

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -1,19 +1,62 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./aside.module.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
-import {  FaHandHoldingHeart,  FaQuestion } from "react-icons/fa";
-import { FaBook, FaPlay, FaMoon, FaSun  } from "react-icons/fa6";
+import { FaHandHoldingHeart, FaQuestion } from "react-icons/fa";
+import { FaBook, FaPlay, FaMoon, FaSun } from "react-icons/fa6";
 import { TbCircleLetterNFilled } from "react-icons/tb";
 
 const Aside = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // return (
+  //   <aside className={styles.container}>
+  //     <ul className={styles.ulList}>
+  //       <li>
+  //         <GiHamburgerMenu />
+  //       </li>
+  //       <li>
+  //         <FaSun />
+  //         <p>Tela Escura</p>
+  //       </li>
+  //       <li>
+  //         <FaPlay />
+  //         <p>Ouvir a Palavra</p>
+  //       </li>
+  //       <li>
+  //         <TbCircleLetterNFilled />
+  //         <p>Explore</p>
+  //       </li>
+  //       <li>
+  //         <FaQuestion />
+  //         <p>Sobre o Projeto</p>
+  //       </li>
+  //       <li>
+  //         <FaHandHoldingHeart />
+  //         <p>Ajude o Projeto</p>
+  //       </li>
+  //       <li>
+  //         <FaBook />
+  //         <p>Livros</p>
+  //       </li>
+  //     </ul>
+  //   </aside>
+  // );
+
+  //
   return (
     <aside className={styles.container}>
-      <ul className={styles.ulList}>
+      <ul className={isOpen ? styles.ulNone : styles.ulFlex}>
         <li>
-          <GiHamburgerMenu />
+          <GiHamburgerMenu className={styles.hamburger} onClick={toggleModal} />
         </li>
         <li>
           <FaSun />
-          <p>Tela Escura</p>
+          <p >Tela Escura</p>
         </li>
         <li>
           <FaPlay />
