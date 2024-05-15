120d491de7b265fb94fb2820b8c3a0ef0e0bc1a9
Diego sales <diegofelipesales23@gmail.com>
2024-05-15 16:42:14 -0400

    refactor:  limpando e refatorando component aside

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -6,52 +6,13 @@ import { FaHandHoldingHeart, FaQuestion } from "react-icons/fa";
 import { FaBook, FaPlay, FaMoon, FaSun } from "react-icons/fa6";
 import { TbCircleLetterNFilled } from "react-icons/tb";
 
-setTimeout(() => {
-  document.querySelector('p').classList.add('visible');
-}, 10000);
-
 const Aside = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
-
-  // return (
-  //   <aside className={styles.container}>
-  //     <ul className={styles.ulList}>
-  //       <li>
-  //         <GiHamburgerMenu />
-  //       </li>
-  //       <li>
-  //         <FaSun />
-  //         <p>Tela Escura</p>
-  //       </li>
-  //       <li>
-  //         <FaPlay />
-  //         <p>Ouvir a Palavra</p>
-  //       </li>
-  //       <li>
-  //         <TbCircleLetterNFilled />
-  //         <p>Explore</p>
-  //       </li>
-  //       <li>
-  //         <FaQuestion />
-  //         <p>Sobre o Projeto</p>
-  //       </li>
-  //       <li>
-  //         <FaHandHoldingHeart />
-  //         <p>Ajude o Projeto</p>
-  //       </li>
-  //       <li>
-  //         <FaBook />
-  //         <p>Livros</p>
-  //       </li>
-  //     </ul>
-  //   </aside>
-  // );
-
-  //
+  
   return (
     <aside className={styles.container}>
       <ul className={isOpen ? styles.modalClose : styles.modalOpen}>
