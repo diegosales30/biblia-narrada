b97738bd5ebc9a6b8190e51f58fac5374fe42765
Diego sales <diegofelipesales23@gmail.com>
2024-05-16 14:10:25 -0400

    fix: testando context global nos demais arquivos

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+"use client";
+import { useContext } from "react";
 import Aside from "@/components/aside";
+import { ModalContext } from "@/context/modal-context";
 import Link from "next/link";
 /* <Link href="/about">about</Link> */
 
 export default function Home() {
+  // const { open, setOpen } = useContext(ModalContext);
+  // console.log(open);
   return <Aside />;
 }
