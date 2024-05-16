51c6791a6b4c560d100f466b45a10c60527db492
Diego sales <diegofelipesales23@gmail.com>
2024-05-16 14:08:36 -0400

    refactor:  add contexto global do modal no arquivo layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,22 @@
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import Provider from "@/context";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata = {
   title: "Bíblia Narrada",
   description: "A Palavra de Deus para todos",
+  keywords: "Bíblia, Narrada, Livros Sagrados, Deus, Jesus Cristo",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <Provider>
+        <body className={inter.className}>{children}</body>
+      </Provider>
     </html>
   );
 }
