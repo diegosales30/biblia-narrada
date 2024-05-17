7bb279c78262607f3305b5e54f0decf067f0a371
Diego sales <diegofelipesales23@gmail.com>
2024-05-17 16:16:18 -0400

    feat: iniciando crianção e edição do componente main

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,14 +1,45 @@
 "use client";
 import styles from "./main.module.scss";
 import Link from "next/link";
+import moses from "../../assets/moses.jpg"
+import jesus from "../../assets/jesus.jpg"
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa6";
 
 const MainComponent = () => {
   return (
     <main className={styles.container}>
-      <h1>versiculo do dia</h1>
-      <h1>O antigo testamento</h1>
-      <h1>O novo testamento</h1>
-      <p><Link href="/about">ABOUTEEEE</Link></p>
+      <div>
+        <h2>Versículo do dia:</h2>
+        <div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            In porro ad soluta alias minus. Recusandae quis ut praesentium eos corporis quam, 
+            cum quia rem. At expedita beatae quibusdam debitis distinction
+          </p>
+          <p>
+            Lorem 5:16
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>O Antigo testamento:</h1>
+        <div>
+          <Link href="/antigo-testamento">
+            <Image src={moses} alt="img antigo testamento" width={150} height={100} />
+            <FaPlay size={50} />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <h1>O Novo testamento:</h1>
+        <div>
+          <Link href="/novo-testamento">
+            <Image src={jesus} alt="img novo testamento" width={150} height={100} />
+            <FaPlay size={50} />
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
