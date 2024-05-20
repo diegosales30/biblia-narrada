0f8f1250220906049b2b777a048651feb74fcd23
Diego sales <diegofelipesales23@gmail.com>
2024-05-20 13:41:04 -0400

    feat: componente versiculo estatico finalizado

--- src/components/versiculos/index.jsx
+++ src/components/versiculos/index.jsx
@@ -3,18 +3,21 @@ import styles from "./versiculos.module.scss";
 const Versiculos = () => {
   return (
     <div className={styles.container}>
-        <h2>Versículo do dia:</h2>
-        <div >
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            In porro ad soluta alias minus. Recusandae quis ut praesentium eos corporis quam, 
-            cum quia rem. At expedita beatae quibusdam debitis distinction
-          </p>
-          <p>
-            Lorem 5:16
-          </p>
-        </div>
+      <h2>Versículo do dia:</h2>
+      <div>
+        <q>
+          Então, foram chamados os escrivães do rei, naquele mesmo tempo e no
+          mês terceiro (que é o mês de sivã), aos vinte e três do mesmo, e se
+          escreveu conforme tudo quanto ordenou Mardoqueu aos judeus, como
+          também aos sátrapas, e aos governadores, e aos maiorais das províncias
+          que se estendem da Índia até à Etiópia, cento e vinte e sete
+          províncias, a cada província segundo a sua escritura e a cada povo
+          conforme a sua língua; como também aos judeus segundo a sua escritura
+          e conforme a sua língua.
+        </q>
+        <p>Ester 8:9</p>
       </div>
+    </div>
   );
 };
 export default Versiculos;
