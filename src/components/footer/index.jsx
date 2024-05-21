53cbabcc5ce8bbcea6cf3ce00d290bdf749a1f15
Diego sales <diegofelipesales23@gmail.com>
2024-05-21 13:36:31 -0400

    feat: criando component footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Este é um rodapé fixo.</p>
+    </footer>
+  );
+};
+
+export default Footer;
