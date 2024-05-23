6792bcc5e06e41bb22d89f2f028affc120e976e5
Diego sales <diegofelipesales23@gmail.com>
2024-05-23 13:01:41 -0400

    feat:  criando database com versiculos do dia estatico

---
+++ src/db/database.js
@@ -0,0 +1,303 @@
+export const versiculos = [
+  {
+    id: 0,
+    verso:
+      "Nisto todos conhecerão que sois meus discípulos, se vos amardes uns aos outros.",
+    livro: "(João 13:35)",
+  },
+  {
+    id: 1,
+    verso:
+      "Porque não temos um sumo sacerdote que não possa compadecer-se das nossas fraquezas; porém um que, como nós, em tudo foi tentado, mas sem pecado.",
+    livro: "(Hebreus 4:15)",
+  },
+  {
+    id: 2,
+    verso:
+      "Porque os meus pensamentos não são os vossos pensamentos, nem os vossos caminhos, os meus caminhos, diz o Senhor.",
+    livro: "(Isaías 55:8)",
+  },
+  {
+    id: 3,
+    verso:
+      "Assim resplandeça a vossa luz diante dos homens, para que vejam as vossas boas obras e glorifiquem o vosso Pai, que está nos céus.",
+    livro: "(Mateus 5:16)",
+  },
+  {
+    id: 4,
+    verso:
+      "É como o óleo precioso sobre a cabeça, que desce sobre a barba, a barba de Arão, e que desce à orla das suas vestes.",
+    livro: "(Salmos 133:2)",
+  },
+  {
+    id: 5,
+    verso:
+      "E Jesus disse-lhe: Amarás o Senhor, teu Deus, de todo o teu coração, e de toda a tua alma, e de todo o teu pensamento.",
+    livro: "(Mateus 22:37)",
+  },
+  {
+    id: 6,
+    verso:
+      "E, tudo quanto fizerdes, fazei-o de todo o coração, como ao Senhor e não aos homens",
+    livro: "(Colossenses 3:23)",
+  },
+  {
+    id: 7,
+    verso:
+      "E, chegando-se Jesus, falou-lhes, dizendo: É-me dado todo o poder no céu e na terra.",
+    livro: "(Mateus 28:18)",
+  },
+  {
+    id: 8,
+    verso:
+      "Não se aparte da tua boca o livro desta Lei; antes, medita nele dia e noite, para que tenhas cuidado de fazer conforme tudo quanto nele está escrito; porque, então, farás prosperar o teu caminho e, então, prudentemente te conduzirás.",
+    livro: "(Josué 1:8)",
+  },
+  {
+    id: 9,
+    verso:
+      "levando ele mesmo em seu corpo os nossos pecados sobre o madeiro, para que, mortos para os pecados, pudéssemos viver para a justiça; e pelas suas feridas fostes sarados..",
+    livro: "(1 Pedro 2:24)",
+  },
+  {
+    id: 10,
+    verso:
+      "Tu conservarás em paz aquele cuja mente está firme em ti; porque ele confia em ti.",
+    livro: "(Isaías 26:3)",
+  },
+  {
+    id: 11,
+    verso:
+      "E em nenhum outro há salvação, porque também debaixo do céu nenhum outro nome há, dado entre os homens, pelo qual devamos ser salvos..",
+    livro: "(Atos 4:12)",
+  },
+  {
+    id: 12,
+    verso:
+      "Porque Deus enviou o seu Filho ao mundo não para que condenasse o mundo, mas para que o mundo fosse salvo por ele.",
+    livro: "(João 3:17)",
+  },
+  {
+    id: 13,
+    verso:
+      "Deleita-te também no Senhor, e ele te concederá o que deseja o teu coração",
+    livro: "(Salmos 37:4)",
+  },
+  {
+    id: 14,
+    verso:
+      "Cheguemos, pois, com confiança ao trono da graça, para que possamos alcançar misericórdia e achar graça, a fim de sermos ajudados em tempo oportuno.",
+    livro: "(Hebreus 4:16)",
+  },
+  {
+    id: 15,
+    verso:
+      "Como o orvalho do Hermom, que desce sobre os montes de Sião; porque ali o Senhor ordena a bênção e a vida para sempre.",
+    livro: "(Salmos 133:3)",
+  },
+  {
+    id: 16,
+    verso:
+      "Tendo por certo isto mesmo: que aquele que em vós começou a boa obra a aperfeiçoará até ao Dia de Jesus Cristo.",
+    livro: "(Filipenses 1:6)",
+  },
+  {
+    id: 17,
+    verso:
+      "pelas quais ele nos tem dado grandíssimas e preciosas promessas, para que por elas fiqueis participantes da natureza divina, havendo escapado da corrupção, que, pela concupiscência, há no mundo,",
+    livro: "(2 Pedro 1:4)",
+  },
+  {
+    id: 18,
+    verso:
+      "não deixando a nossa congregação, como é costume de alguns; antes, admoestando-nos uns aos outros; e tanto mais quanto vedes que se vai aproximando aquele Dia.",
+    livro: "(Hebreus 10:25)",
+  },
+  {
+    id: 19,
+    verso:
+      "nem a altura, nem a profundidade, nem alguma outra criatura nos poderá separar do amor de Deus, que está em Cristo Jesus, nosso Senhor!",
+    livro: "(Romanos 8:39)",
+  },
+  {
+    id: 20,
+    verso:
+      "Porque estou certo de que nem a morte, nem a vida, nem os anjos, nem os principados, nem as potestades, nem o presente, nem o porvir,",
+    livro: "(Romanos 8:38)",
+  },
+  {
+    id: 21,
+    verso: "sabendo que a prova da vossa fé produz a paciência.",
+    livro: "(Tiago 1:3)",
+  },
+  {
+    id: 22,
+    verso:
+      "Bem-aventurado o varão que sofre a tentação; porque, quando for provado, receberá a coroa da vida, a qual o Senhor tem prometido aos que o amam.",
+    livro: "(Tiago 1:12)",
+  },
+  {
+    id: 23,
+    verso:
+      "Mas a todos quantos o receberam deu-lhes o poder de serem feitos filhos de Deus: aos que creem no seu nome,",
+    livro: "(João 1:12)",
+  },
+  {
+    id: 24,
+    verso: "De sorte que a fé é pelo ouvir, e o ouvir pela palavra de Deus",
+    livro: "(Romanos 10:17)",
+  },
+  {
+    id: 25,
+    verso:
+      "Ele te declarou, ó homem, o que é bom; e que é o que o Senhor pede de ti, senão que pratiques a justiça, e ames a beneficência, e andes humildemente com o teu Deus?”",
+    livro: "(Miquéias 6:8)",
+  },
+  {
+    id: 26,
+    verso:
+      "“Ninguém tem maior amor do que este: de dar alguém a sua vida pelos seus amigos.",
+    livro: "(João 15:13)",
+  },
+  {
+    id: 27,
+    verso:
+      "Porque a palavra de Deus é viva, e eficaz, e mais penetrante do que qualquer espada de dois gumes, e penetra até à divisão da alma, e do espírito, e das juntas e medulas, e é apta para discernir os pensamentos e intenções do coração..",
+    livro: "(Hebreus 4:12)",
+  },
+  {
+    id: 28,
+    verso:
+      "Deixo-vos a paz, a minha paz vos dou; não vo-la dou como o mundo a dá. Não se turbe o vosso coração, nem se atemorize.",
+    livro: "(João 14:27)",
+  },
+  {
+    id: 29,
+    verso: "Oh! Quão bom e quão suave é que os irmãos vivam em união!",
+    livro: "(Salmos 133:1)",
+  },
+  {
+    id: 30,
+    verso:
+      "Conhecemos o amor nisto: que ele deu a sua vida por nós, e nós devemos dar a vida pelos irmãos.",
+    livro: "(1 João 3:16)",
+  },
+  {
+    id: 31,
+    verso:
+      "Ou não sabeis que o nosso corpo é o templo do Espírito Santo, que habita em vós, proveniente de Deus, e que não sois de vós mesmos?",
+    livro: "(1 Coríntios 6:19)",
+  },
+  {
+    id: 32,
+    verso:
+      "No princípio, era o Verbo, e o Verbo estava com Deus, e o Verbo era Deus.”",
+    livro: "(João 1:1)",
+  },
+  {
+    id: 33,
+    verso:
+      "O meu Deus, segundo as suas riquezas, suprirá todas as vossas necessidades em glória, por Cristo Jesus.",
+    livro: "(Filipenses 4:19)",
+  },
+  {
+    id: 34,
+    verso:
+      "Ora, estes foram mais nobres do que os que estavam em Tessalônica, porque de bom grado receberam a palavra, examinando cada dia nas Escrituras se estas coisas eram assim.",
+    livro: "(Atos 17:11)",
+  },
+  {
+    id: 35,
+    verso:
+      "Confessai as vossas culpas uns aos outros e orai uns pelos outros, para que sareis; a oração feita por um justo pode muito em seus efeitos.",
+    livro: "(Tiago 5:16)",
+  },
+  {
+    id: 36,
+    verso:
+      "Portanto, nós também, pois, que estamos rodeados de uma tão grande nuvem de testemunhas, deixemos todo embaraço e o pecado que tão de perto nos rodeia e corramos, com paciência, a carreira que nos está proposta,",
+    livro: "(Hebreus 12:1)",
+  },
+  {
+    id: 37,
+    verso:
+      "Revesti-vos, pois, como eleitos de Deus, santos e amados, de entranhas de misericórdia, de benignidade, humildade, mansidão, longanimidade",
+    livro: "(Colossenses 3:12)",
+  },
+  {
+    id: 38,
+    verso:
+      "E criou Deus o homem à sua imagem; à imagem de Deus o criou; macho e fêmea os criou.",
+    livro: "(Gênesis 1:27)",
+  },
+  {
+    id: 39,
+    verso: "Porque o meu jugo é suave, e o meu fardo é leve.",
+    livro: "(Mateus 11:30)",
+  },
+  {
+    id: 40,
+    verso:
+      "Ora, àquele que é poderoso para fazer tudo muito mais abundantemente além daquilo que pedimos ou pensamos, segundo o poder que em nós opera,",
+    livro: "(Efésios 3:20)",
+  },
+  {
+    id: 41,
+    verso:
+      "E disse-lhes Pedro: Arrependei-vos, e cada um de vós seja batizado em nome de Jesus Cristo para perdão dos pecados, e recebereis o dom do Espírito Santo.",
+    livro: "(Atos 2:38)",
+  },
+  {
+    id: 42,
+    verso:
+      "Todos nós andamos desgarrados como ovelhas; cada um se desviava pelo seu caminho, mas o Senhor fez cair sobre ele a iniquidade de nós todos.",
+    livro: "(Isaías 53:6)",
+  },
+  {
+    id: 43,
+    verso: "Meus irmãos, tende grande gozo quando cairdes em várias tentações,",
+    livro: "(Tiago 1:2)",
+  },
+  {
+    id: 44,
+    verso:
+      "Na verdade, na verdade vos digo que quem ouve a minha palavra e crê naquele que me enviou tem a vida eterna e não entrará em condenação, mas passou da morte para a vida.",
+    livro: "(João 5:24)",
+  },
+  {
+    id: 45,
+    verso:
+      "Ora, sem fé é impossível agradar-lhe, porque é necessário que aquele que se aproxima de Deus creia que ele existe e que é galardoador dos que o buscam",
+    livro: "(Hebreus 11:6)",
+  },
+  {
+    id: 46,
+    verso:
+      "Disse-lhe Jesus: Eu sou a ressurreição e a vida; quem crê em mim, ainda que esteja morto, viverá;",
+    livro: "(João 11:25)",
+  },
+  {
+    id: 47,
+    verso:
+      "Ora, o Deus de esperança vos encha de todo o gozo e paz em crença, para que abundeis em esperança pela virtude do Espírito Santo.",
+    livro: "(Romanos 15:13)",
+  },
+  {
+    id: 48,
+    verso:
+      "Àquele que não conheceu pecado, o fez pecado por nós; para que, nele, fôssemos feitos justiça de Deus.",
+    livro: "(2 Coríntios 5:21)",
+  },
+  {
+    id: 49,
+    verso:
+      "Verdadeiramente, ele tomou sobre si as nossas enfermidades e as nossas dores levou sobre si; e nós o reputamos por aflito, ferido de Deus e oprimido.",
+    livro: "(Isaías 53:4)",
+  },
+  {
+    id: 50,
+    verso:
+      "Porque Deus não nos deu o espírito de temor, mas de fortaleza, e de amor, e de moderação",
+    livro: "(2 Timóteo 1:7)",
+  },
+];
